4d0c34084e29410f3b732e8eb76bd12b3931b0ec
YunusGoven <93343010+YunusGoven@users.noreply.github.com>
2024-12-05 14:02:15 +0100

    series pages: show owned books done

--- lib/screen/series_details_page.dart
+++ lib/screen/series_details_page.dart
@@ -22,21 +22,33 @@ class SeriesDetailsPage extends StatelessWidget {
           children: [
             Row(
               children: [
-                Image.network(series.cover! , width: 100, height: 150,),
-                SizedBox(width: 24,),
-                Column(
-                  children: [
-                    _buildHeaderSection(context),
-                    SizedBox(height: 10,),
-                    _buildGenresSection(context),
-                    SizedBox(height: 10,),
-                  ],
+                Image.network(
+                  series.cover!,
+                  width: 100,
+                  height: 150,
+                ),
+                SizedBox(
+                  width: 14,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildHeaderSection(context),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      _buildGenresSection(context),
+                    ],
+                  ),
                 ),
               ],
             ),
             Divider(),
             _buildAuthorsSection(context),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Divider(),
             _buildBooksSection(context)
           ],
@@ -46,8 +58,10 @@ class SeriesDetailsPage extends StatelessWidget {
   }
 
   Widget _buildHeaderSection(context) {
-    return Text(series.title!,                             
-      style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+    return Text(
+      series.title!,
+      style: const TextStyle(
+          fontWeight: FontWeight.bold, fontSize: 24, letterSpacing: 0),
     );
   }
 
@@ -55,18 +69,18 @@ class SeriesDetailsPage extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Catégories", style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
+        Text("Catégories",
+            style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
         Wrap(
-          spacing: 8,
-          runSpacing: 8,
-          alignment: WrapAlignment.start,
-          crossAxisAlignment: WrapCrossAlignment.start,
-          direction: Axis.horizontal,
-          runAlignment: WrapAlignment.start,
-          verticalDirection: VerticalDirection.down,
-          clipBehavior: Clip.none,
-          children: _getGenresWidget()
-         ),
+            spacing: 8,
+            runSpacing: 8,
+            alignment: WrapAlignment.start,
+            crossAxisAlignment: WrapCrossAlignment.start,
+            direction: Axis.horizontal,
+            runAlignment: WrapAlignment.start,
+            verticalDirection: VerticalDirection.down,
+            clipBehavior: Clip.none,
+            children: _getGenresWidget()),
       ],
     );
   }
@@ -81,10 +95,13 @@ class SeriesDetailsPage extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
           ),
           child: Padding(
-            padding: EdgeInsetsDirectional.symmetric(horizontal: 10, vertical: 5),
+            padding:
+                EdgeInsetsDirectional.symmetric(horizontal: 10, vertical: 5),
             child: Text(
               genre,
-              style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),),
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+            ),
           ),
         ),
       );
@@ -93,44 +110,160 @@ class SeriesDetailsPage extends StatelessWidget {
     return widgetList;
   }
 
-  Widget _buildAuthorsSection(context){
+  Widget _buildAuthorsSection(context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Contributeurs", style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
+        Text("Contributeurs",
+            style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
         Wrap(
-          spacing: 8,
-          runSpacing: 8,
-          alignment: WrapAlignment.start,
-          crossAxisAlignment: WrapCrossAlignment.start,
-          direction: Axis.horizontal,
-          runAlignment: WrapAlignment.start,
-          verticalDirection: VerticalDirection.down,
-          clipBehavior: Clip.none,
-          children: _getGenresWidget()
-         ),
+            spacing: 8,
+            runSpacing: 8,
+            alignment: WrapAlignment.start,
+            crossAxisAlignment: WrapCrossAlignment.start,
+            direction: Axis.horizontal,
+            runAlignment: WrapAlignment.start,
+            verticalDirection: VerticalDirection.down,
+            clipBehavior: Clip.none,
+            children: _getAuthorsWidget()),
       ],
-    );;
+    );
+  }
+
+  List<Widget> _getAuthorsWidget() {
+    List<Widget> widgetList = [];
+    for (var author in series.authorsList!) {
+      widgetList.add(
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[600],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding:
+                EdgeInsetsDirectional.symmetric(horizontal: 10, vertical: 5),
+            child: Column(
+              children: [
+                Text(
+                  author.fullName,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  author.role,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w300),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    return widgetList;
   }
 
   Widget _buildBooksSection(context) {
-    
     return Container(
-      child: ListView.builder(
-        shrinkWrap: true,
-        itemCount: series.books.length,
-        itemBuilder: (_,index) {
-          var i = NetworkImage(series.books[index].cover!, headers: {'Access-Control-Allow-Origin':'*'});
-
-          return Container(child: Column(
-            children: [
-              Image(image: i),
-              Text(series.books[index].title!),
-            ],
-          ),);
-        }
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Éditions",
+              style:
+                  const TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
+          Text('${series.nbBooks!} tomes',
+              style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                  color: Colors.grey)),
+          SizedBox(
+            height: 10,
+          ),
+          _buildBooks()
+        ],
       ),
     );
+  }
+
+  Widget _buildBooks() {
+    return ListView.builder(
+        controller: ScrollController(),
+        shrinkWrap: true,
+        itemCount: series.books.length,
+        itemBuilder: (_, index) {
+          var i = NetworkImage(series.books[index].cover!,
+              headers: {'Access-Control-Allow-Origin': '*'});
+          var fullTitle = series.books[index].title ?? 'Titre inconnu';
+          var title, tome;
+          if (fullTitle.contains(' - ')) {
+            var parts = fullTitle.split(' - ');
+            title = parts[0];
+            tome = parts.length > 1 ? parts[1] : '';
+          } else {
+            title = fullTitle;
+            tome = '';
+          }
+          var readingStatus = series.books[index].readingStatus!;
+          return InkWell(
+            onTap: () {
+              print(title);
+            },
+            child: Container(
+              padding: const EdgeInsets.only(bottom: 16),
+              decoration: BoxDecoration(
+                color: Colors.white60,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                Expanded(
+                  child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Image(
+                            image: i,
+                            width: 80,
+                            height: 120,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                title,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w600, fontSize: 22),
+                              ),
+                              Text(
+                                tome,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w400, fontSize: 18),
+                              ),
+                              Text(
+                                readingStatus,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w200, fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ]),
+                ),
+                const Icon(
+                  Icons.chevron_right,
+                  size: 24,
+                ),
+              ]),
+            ),
+          );
+        });
   }
 
   AppBar _appBarBuilder(BuildContext context) {
